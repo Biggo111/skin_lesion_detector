137bd785fe2741723340ff80fb3084fa535d0b60
Biggo111 <routh.biggo123@gmail.com>
2024-03-15 17:10:54 +0600

    fixed

--- lib/feature/home/screens/home_screen.dart
+++ lib/feature/home/screens/home_screen.dart
@@ -23,81 +23,117 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
       appBar: AppBar(
         title: const Text('Skin Lesion Detector'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            if(resultState is SuccessState)
-            ...[
-           Container(
-              height: 50,
-              width: 200,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.deepPurple, width: 2),
-              ),
-              child: Center(
-                child: Text(
-                  "${ref.watch(homeControllerProvider.notifier).lesionType}",
-                  style: const TextStyle(fontSize: 16, color: Colors.deepPurple),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          await ref.watch(homeControllerProvider.notifier).reset();
+        },
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20),
+                if (resultState is SuccessState) ...[
+                  Container(
+                      height: 50,
+                      width: 360,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.indigo, width: 2),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "${ref.watch(homeControllerProvider.notifier).lesionType}",
+                          style: const TextStyle(
+                              fontSize: 16, color: Colors.indigo),
+                        ),
+                      )),
+                  const SizedBox(height: 20),
+                  KButton(
+                    title: "See details",
+                    onPressedCallback: () async {
+                      Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) => const LesionDetailsScreen()));
+                    },
+                    width: 360,
+                    color: Colors.indigo,
+                    textStyle: const TextStyle(fontSize: 16, color: Colors.white),
+                  )
+                ],
+                const SizedBox(
+                  height: 50,
                 ),
-              )
-            ),
-            const SizedBox(height: 20),
-            KButton(
-              title: "See details",
-              onPressedCallback: ()async{
-                Navigator.push(context, CupertinoPageRoute(builder: (context)=> const LesionDetailsScreen()));
-              },
-              color: Colors.deepPurple,
-              textStyle: const TextStyle(fontSize: 16, color: Colors.white),
-            )
-            ],
-            const SizedBox(height: 20,),
-            if(selectedImageState is ImageCapturedState)
-              Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Colors.deepPurple, width: 2),
+                Container(
+                    height: 300,
+                    width: 360,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.indigo, width: 2),
+                    ),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: selectedImageState is ImageCapturedState || selectedImageState is SuccessState
+                            ? Image.file(
+                                ref
+                                    .watch(homeControllerProvider.notifier)
+                                    .takenImage!,
+                                fit: BoxFit.cover)
+                            : const Icon(
+                                Icons.image,
+                                size: 100,
+                                color: Colors.indigo,
+                              ))),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    KButton(
+                      title: "Take a picture",
+                      onPressedCallback: () async {
+                        await ref
+                            .watch(homeControllerProvider.notifier)
+                            .takePicuture();
+                      },
+                      color: Colors.indigo,
+                      textStyle: const TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                    const SizedBox(height: 20),
+                    KButton(
+                      title: "Upload a picture",
+                      onPressedCallback: () async {
+                        await ref
+                            .watch(homeControllerProvider.notifier)
+                            .takePicuture(uploadImage: true);
+                      },
+                      color: Colors.indigo,
+                      textStyle: const TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ],
                 ),
-                child: ClipRRect( 
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.file(ref.watch(homeControllerProvider.notifier).takenImage!, fit: BoxFit.cover))
-              ),
-            const SizedBox(height: 20),
-            KButton(
-              title: "Take a picture",
-              onPressedCallback: ()async{
-                await ref.watch(homeControllerProvider.notifier).takePicuture();
-              },
-              color: Colors.deepPurple,
-              textStyle: const TextStyle(fontSize: 16, color: Colors.white),
+                const SizedBox(height: 80),
+                KButton(
+                  title: "Check",
+                  onPressedCallback: () async {
+                    if (ref.watch(homeControllerProvider.notifier).takenImage ==
+                        null) {
+                      ToastService.customToast(
+                          "Please take or upload a picture first!");
+                    } else {
+                      await ref
+                        .watch(homeControllerProvider.notifier)
+                        .fatchLesionType();
+                    }
+                  },
+                  width: 360,
+                  color: Colors.indigo,
+                  textStyle: const TextStyle(fontSize: 16, color: Colors.white),
+                )
+              ],
             ),
-            const SizedBox(height: 20),
-            KButton(
-              title: "Upload a picture",
-              onPressedCallback: ()async{
-                await ref.watch(homeControllerProvider.notifier).takePicuture(uploadImage: true);
-              },
-              color: Colors.deepPurple,
-              textStyle: const TextStyle(fontSize: 16, color: Colors.white),
-            ),
-            const SizedBox(height: 50),
-            KButton(
-              title: "Check",
-              onPressedCallback: ()async{
-                if(ref.watch(homeControllerProvider.notifier).takenImage == null){
-                  ToastService.customToast("Please take or upload a picture first!");
-                }
-                await ref.watch(homeControllerProvider.notifier).fatchLesionType();
-              },
-              color: Colors.deepPurple,
-              textStyle: const TextStyle(fontSize: 16, color: Colors.white),
-            )
-          ],
+          ),
         ),
       ),
     );
